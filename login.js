d0f6b738826823213d2107b375db9f1555b7dce8
Sabbir Ahmed <61541368+SAshourav@users.noreply.github.com>
2023-01-14 03:58:28 +0600

    Update login.js

--- login.js
+++ login.js
@@ -3,6 +3,6 @@ document.getElementById("login-submit").addEventListener("click", function(){
     const user_password = document.getElementById("user-password").value;
     if (user_email == "sabbir@gmail.com" && user_password == "1234"){
         console.log("valid user");
-        window.location.href="http://127.0.0.1:5500/project%20BANK/home.html"
+        window.location.href="home.html"
     }
 })
