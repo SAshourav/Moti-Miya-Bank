f61a90aa9d56ca77407d80af9cca5794edd9a409
Sabbir Ahmed <61541368+SAshourav@users.noreply.github.com>
2023-01-14 05:18:40 +0600

    Update login.js

--- login.js
+++ login.js
@@ -3,6 +3,6 @@ document.getElementById("login-submit").addEventListener("click", function(){
     const user_password = document.getElementById("user-password").value;
     if (user_email == "sabbir@gmail.com" && user_password == "1234"){
         console.log("valid user");
-        window.location.href="home.html"
+        location.href="home.html"
     }
 })
